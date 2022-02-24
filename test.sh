6ce49dfffd45a4a8956df59a58cbfb7a98fe0e02
Joshua Rutschmann <joshua.rutschmann@gmx.de>
2022-02-24 22:50:15 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-touch "test/$(date +%s)"
-count=$(ls test | wc -l)
-echo "$count"
-if [ $count -gt 10 ] ; then
-  echo "Deleting oldest"
-  rm $(ls -vd test/* | head -n 1)
-else
-  echo "Not full"
-fi
